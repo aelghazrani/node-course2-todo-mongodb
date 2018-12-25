c13747c9eb0292c6167f3afee0149ddfed1f0226
ELGHAZRANI Abdelali <elghazrani.abdelali@gmail.com>
2018-12-25 19:01:47 +0000

    Add User Modal

--- server/server.js
+++ server/server.js
@@ -9,24 +9,39 @@ mongoose.connect(url, { useNewUrlParser: true });
 //make a new modal
 var Todo = mongoose.model('Todo',{
     text:{
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt :{
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 //Instanciate the modal
-var newTodo = new Todo({
-    text: 'new note maked with Mongoose package.',
-    completed: true,
-    completedAt: new Date().getTime()
+// var newTodo = new Todo({
+//     text: '    new note maked with space at begin and end of statement     '    
+// });
+
+var User = mongoose.model('User', {
+    email:{
+        type: String,
+        required: true,
+        minlength: 10,
+        trim: true
+    }
 });
 
-newTodo.save().then((doc) => {
+var newUser = new User({
+    email: 'elghazrani.abdelali@cbc.org'
+});
+newUser.save().then((doc) => {
     console.log(doc);
 }, (e) => {
     console.log('Error : ', e);
